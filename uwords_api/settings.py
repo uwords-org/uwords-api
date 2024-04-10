1b4493b0f0c3f11a938a243823fbc4cf32807bd8
Danone <mr.super.ko@inbox.ru>
2024-04-10 14:54:13 +0600

    upgrade minio + add celery tasks

--- uwords_api/settings.py
+++ uwords_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-7@kxo^na=s()b(d=#$=-v3uaq=qc)bmk(z6ngr1%wyr5oy%7s3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
